1fdca4bb31fe718879c766650d3339ee912235df
Natalia Kirieieva <nataliakireewa@gmail.com>
2024-02-27 18:19:06 +0200

    url for local and prod upd

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     NEXT_LOCAL_API_BASE_URL: "http://localhost:3004",
-    NEXT_PUBLIC_API_BASE_URL: "https://bbqrki.vercel.app",
+    NEXT_PUBLIC_API_BASE_URL: "https://www.bbqr.site",
   },
 };
 
