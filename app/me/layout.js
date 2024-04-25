b843708504c286fb4a072f3feddf0e60b9f104bf
Natalia Kirieieva <nataliakireewa@gmail.com>
2024-04-25 19:15:10 +0300

    favicon added to me page

--- app/me/layout.js
+++ app/me/layout.js
@@ -1,5 +1,15 @@
 import "@styles/me.css";
 
+export const metadata = {
+  title: {
+    default: "Nataliaki",
+  },
+  description: "Web Developer Profile!",
+  icons: {
+    icon: "/nk.png",
+  },
+};
+
 export default function MeLayout({ children }) {
   return <>{children}</>;
 }
